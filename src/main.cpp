7736e95a8633565ee7911362a3b485837f15e7e4
llb1989 <lyla@westernmech.ca>
2025-09-22 18:51:01 -0600

    it works

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "main.h"
+#include "pros/abstract_motor.hpp"
 #include "pros/llemu.hpp"
 #include "pros/misc.h"
 #include "pros/motors.h"
 #include "pros/motors.hpp"
+#include "pros/rtos.hpp"
+#include <cmath>
 
 /**
  * A callback function for LLEMU's center button.
@@ -11,32 +14,15 @@
  * "I was pressed!" and nothing.
  */
 
-pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Controller master(pros::E_CONTROLLER_MASTER); // idk why we did it like this but this is conttoller
 pros::MotorGroup left_mg({-18, 20, -16});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 pros::MotorGroup right_mg({12, -14, 15});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-/* pros::Motor dtmotor1(1);  
-pros::Motor dtmotor2(1);
-pros::Motor dtmotor3(1);
-pros::Motor dtmotor4(1);
-pros::Motor dtmotor5(1);
-pros::Motor dtmotor6(1); */
-pros::Motor intmotor1(1);
-pros::Motor intmotor2(-2);
-pros::Motor intmotor3(9);
 
+pros::Motor intmotor1(1); // bottom roller?
+pros::Motor intmotor2(-2); // middle roller?
+pros::Motor intmotor3(9); // top roller
 
-/*
-	void setIntakeHigh(int intakePower){
-		intmotor1.move(intakePower);
-		intmotor2.move(intakePower);
-		intmotor3.move(intakePower);
-}
-
-	void setIntakeMid(int intakePower){
-		intmotor1.move(intakePower);
-		intmotor2.move(-intakePower);
-		intmotor3.move(intakePower);
-} */
+bool locktoggle = false;
 
 void on_center_button() {
 	static bool pressed = false;
@@ -111,6 +97,7 @@ void autonomous() {}
 void opcontrol() {
 	
 	while (true) {
+		
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
@@ -119,67 +106,54 @@ void opcontrol() {
 		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
 		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
 		left_mg.move(dir + turn);                      // Sets left motor voltage
-		right_mg.move(dir - turn);                     // Sets right motor voltage
+		right_mg.move(dir - turn);                     // Sets right motor voltage	
 
-		bool locktoggle = true;
-		if(master.get_digital(pros::E_CONTROLLER_DIGITAL_A)){
-			locktoggle = !locktoggle;
-		}
+	if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) { // turn lock on and off
+      locktoggle = !locktoggle; 
+	}
 
-	// if (locktoggle = false){ // if button pressed
-		if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1) && (!locktoggle)){
-			intmotor1.move_voltage(12000);
-			intmotor2.move_voltage(12000);
-			intmotor3.move_voltage(12000);
-		} else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1) && (!locktoggle)) {
-			intmotor1.move_voltage(12000);
-			intmotor2.move_voltage(12000);
-			intmotor3.move_voltage(-12000);
-		} else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L2) && (!locktoggle)) {
-			intmotor1.move_voltage(-12000);
-			intmotor2.move_voltage(-12000);
-			intmotor3.move_voltage(-12000);
-		} else {
-			intmotor1.move_voltage(0);
-			intmotor2.move_voltage(0);
-			intmotor3.move_voltage(0);
-		}
+	if (locktoggle) {
+        if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
+            intmotor1.move_voltage(12000);
+            intmotor2.move_voltage(12000);
+            intmotor3.move_voltage(0);
+			intmotor3.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
+        } else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+            intmotor1.move_voltage(12000);
+            intmotor2.move_voltage(12000);
+            intmotor3.move_voltage(0);
+			intmotor3.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
+        } else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+            intmotor1.move_voltage(-12000);
+            intmotor2.move_voltage(-12000);
+            intmotor3.move_voltage(0);
+			intmotor3.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
+        } else {
+            intmotor1.move_voltage(0);
+            intmotor2.move_voltage(0);
+            intmotor3.move_voltage(0); // stop moving
+        }
 
-		if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1) && (locktoggle)){
-			intmotor1.move_voltage(12000);
-			intmotor2.move_voltage(12000);
-			intmotor3.move_voltage(0);
-		} else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1) && (locktoggle)) {
-			intmotor1.move_voltage(12000);
-			intmotor2.move_voltage(12000);
-			intmotor3.move_voltage(0);
-		} else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L2) && (locktoggle)) {
-			intmotor1.move_voltage(-12000);
-			intmotor2.move_voltage(-12000);
-			intmotor3.move_voltage(0);
-		} else {
-			intmotor1.move_voltage(0);
-			intmotor2.move_voltage(0);
-			intmotor3.move_voltage(0); // stop moving
-		}
-
-		
-		/* if(pros::E_CONTROLLER_DIGITAL_R1){
-			setIntakeHigh(127);
-		} else if (pros::E_CONTROLLER_DIGITAL_X) {
-			setIntakeHigh(-127);
-		} else {
-			setIntakeHigh(0);
-		}
-
-		if(pros::E_CONTROLLER_DIGITAL_L1){
-			setIntakeMid(127);
-		} else if (pros::E_CONTROLLER_DIGITAL_X) {
-			setIntakeHigh(-127);
-		} else {
-			setIntakeMid(0);
-		} */
-
+    } else {
+        
+		if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
+            intmotor1.move_voltage(12000);
+            intmotor2.move_voltage(12000);
+            intmotor3.move_voltage(12000);
+        } else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+            intmotor1.move_voltage(12000);
+            intmotor2.move_voltage(12000);
+            intmotor3.move_voltage(-12000);
+        } else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+            intmotor1.move_voltage(-12000);
+            intmotor2.move_voltage(-12000);
+            intmotor3.move_voltage(-12000);
+        } else {
+            intmotor1.move_voltage(0);
+            intmotor2.move_voltage(0);
+            intmotor3.move_voltage(0); // stop moving
+        }
+    }
+	}
 		pros::delay(20);                               // Run for 20 ms then update
 	}
-}
