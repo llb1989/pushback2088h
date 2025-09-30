b6d888bd3b4db0d1495590638f793b34f060ab21
llb1989 <lyla@westernmech.ca>
2025-09-29 19:47:41 -0600

    add lemlib

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "lemlib/chassis/trackingWheel.hpp"
 #include "pros/abstract_motor.hpp"
 #include "pros/llemu.hpp"
 #include "pros/misc.h"
@@ -6,6 +7,7 @@
 #include "pros/motors.hpp"
 #include "pros/rtos.hpp"
 #include <cmath>
+#include "lemlib/api.hpp" // IWYU pragma: keep
 
 /**
  * A callback function for LLEMU's center button.
@@ -14,15 +16,63 @@
  * "I was pressed!" and nothing.
  */
 
-pros::Controller master(pros::E_CONTROLLER_MASTER); // idk why we did it like this but this is conttoller
+pros::Controller master(pros::E_CONTROLLER_MASTER);
 pros::MotorGroup left_mg({-18, 20, -16});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 pros::MotorGroup right_mg({12, -14, 15});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
-pros::Motor intmotor1(1); // bottom roller?
-pros::Motor intmotor2(-2); // middle roller?
-pros::Motor intmotor3(9); // top roller
+pros::Motor intmotor1(1); // bottom roller
+pros::Motor intmotor2(-2); // middle roller
+pros::Motor intmotor3(9); // top 
+
+pros::Imu imu(19); // make real port
 
 bool locktoggle = false;
+bool slowtoggle = false;
+
+lemlib::Drivetrain drivetrain(&left_mg, // left motor group
+                              &right_mg, // right motor group
+                              12, // 10 inch track width
+                              lemlib::Omniwheel::NEW_325, // using new 4" omnis
+                              450, // drivetrain rpm is 360
+                              2 // horizontal drift is 2 (for now)
+);
+
+lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            nullptr, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors // odometry sensors
+);
 
 void on_center_button() {
 	static bool pressed = false;
@@ -41,10 +91,19 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+    // print position to brain screen
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
 }
 
 /**
@@ -76,7 +135,26 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+
+    int autonumber = 1;
+    switch (autonumber) {
+
+    case 1:
+    // set position to x:0, y:0, heading:0
+    chassis.setPose(0, 0, 0);
+    // turn to face heading 90 with a very long timeout
+    chassis.turnToHeading(90, 100000);
+    break;
+
+    }
+
+    
+
+
+
+
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -116,20 +194,28 @@ void opcontrol() {
       locktoggle = !locktoggle; 
 	}
 
-	if (locktoggle) {
+    if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) { // turn lock on and off
+      slowtoggle = !slowtoggle; 
+	}
+
+    if (locktoggle && slowtoggle) {
+
         if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
-            intmotor1.move_voltage(12000);
+            intmotor1.move_voltage(6000);
             intmotor2.move_voltage(0);
+            intmotor2.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
             intmotor3.move_voltage(0);
 			intmotor3.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
         } else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
-            intmotor1.move_voltage(12000);
+            intmotor1.move_voltage(6000);
             intmotor2.move_voltage(0);
+            intmotor2.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
             intmotor3.move_voltage(0);
 			intmotor3.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
         } else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
-            intmotor1.move_voltage(-12000);
+            intmotor1.move_voltage(-6000);
             intmotor2.move_voltage(0);
+            intmotor2.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
             intmotor3.move_voltage(0);
 			intmotor3.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
         } else {
@@ -138,9 +224,55 @@ void opcontrol() {
             intmotor3.move_voltage(0); // stop moving
         }
 
+    } else if (locktoggle) {
+
+        if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
+            intmotor1.move_voltage(12000);
+            intmotor2.move_voltage(0);
+            intmotor2.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
+            intmotor3.move_voltage(0);
+			intmotor3.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
+        } else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+            intmotor1.move_voltage(12000);
+            intmotor2.move_voltage(0);
+            intmotor2.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
+            intmotor3.move_voltage(0);
+			intmotor3.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
+        } else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+            intmotor1.move_voltage(-12000);
+            intmotor2.move_voltage(0);
+            intmotor2.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
+            intmotor3.move_voltage(0);
+			intmotor3.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // lock 
+        } else {
+            intmotor1.move_voltage(0);
+            intmotor2.move_voltage(0);
+            intmotor3.move_voltage(0); // stop moving
+        }
+
+    } else if (slowtoggle) {
+
+        if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
+            intmotor1.move_voltage(6000);
+            intmotor2.move_voltage(6000);
+            intmotor3.move_voltage(6000);
+        } else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+            intmotor1.move_voltage(6000);
+            intmotor2.move_voltage(6000);
+            intmotor3.move_voltage(-6000);
+        } else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+            intmotor1.move_voltage(-6000);
+            intmotor2.move_voltage(-6000);
+            intmotor3.move_voltage(-6000);
+        } else {
+            intmotor1.move_voltage(0);
+            intmotor2.move_voltage(0);
+            intmotor3.move_voltage(0); // stop moving
+        }
+
     } else {
-        
-		if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
+    
+        if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
             intmotor1.move_voltage(12000);
             intmotor2.move_voltage(12000);
             intmotor3.move_voltage(12000);
@@ -157,6 +289,7 @@ void opcontrol() {
             intmotor2.move_voltage(0);
             intmotor3.move_voltage(0); // stop moving
         }
+
     }
 	}
 		pros::delay(20);                               // Run for 20 ms then update
