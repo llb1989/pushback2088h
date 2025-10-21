bef9e727001b6f58833300f1476a89b910a3d91e
Nolan Li <nolan.lipishan@gmail.com>
2025-10-20 19:06:33 -0600

    auto select im so awesome and smart!

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 
 // motor groups
-pros::MotorGroup leftMotors({18, -20, 16}, pros::MotorGearset::blue);    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-pros::MotorGroup rightMotors({-12, 14, -17}, pros::MotorGearset::blue);  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+pros::MotorGroup leftMotors({-18, 20, -16}, pros::MotorGearset::blue);    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+pros::MotorGroup rightMotors({12, -14, 17}, pros::MotorGearset::blue);  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
 //intake mototrso
 pros::Motor intmotor1(1); // bottom roller
@@ -18,6 +18,18 @@ pros::Imu imu(19);
 
 pros::adi::Pneumatics littlewill('A', false);
 pros::adi::Pneumatics fakeewill('B', false);
+
+    const int numAutos = 4;
+//These are in  NOT degrees
+int states[numAutos] = {0, 1, 2, 3};
+int currAuto = 0;
+
+void nextState() {
+    currAuto += 1;
+    if (currAuto == numAutos) {
+        currAuto = 1;
+    }
+}
 
 void intakeall(int intakepower) {
             intmotor1.move_voltage(intakepower);
@@ -107,9 +119,10 @@ lemlib::ControllerSettings angularController(2, // proportional gain (kP)
 // sensors for odometry
 lemlib::OdomSensors sensors(
     // &vertical, // vertical tracking wheel
-                            // nullptr, // vertical tracking wheel 2, set to nullptr as we don't have a second one
-                            // // &horizontal, // horizontal tracking wheel
-                            // nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            nullptr,
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we don't have a second one
+                            nullptr, // &horizontal, // horizontal tracking wheel
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
                             &imu // inertial sensor
 );
 
@@ -137,7 +150,6 @@ lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors
 void initialize() {
     pros::lcd::initialize(); // initialize brain screen
     chassis.calibrate(); // calibrate sensors
-
     // the default rate is 50. however, if you need to change the rate, you
     // can do the following.
     // lemlib::bufferedStdout().setRate(...);
@@ -153,6 +165,7 @@ void initialize() {
             pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
             pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
             pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            pros::lcd::print(3, "Auto: %d", currAuto);
             // log position telemetry
             lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
             // delay to save resources
@@ -181,32 +194,23 @@ ASSET(example_txt); // '.' replaced with "_" to make c++ happy
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-    // Move to x: 20 and y: 15, and face heading 90. Timeout set to 4000 ms
-    chassis.moveToPose(20, 15, 90, 4000);
-    // Move to x: 0 and y: 0 and face heading 270, going backwards. Timeout set to 4000ms
-    chassis.moveToPose(0, 0, 270, 4000, {.forwards = false});
-    // cancel the movement after it has traveled 10 inches
-    chassis.waitUntil(10);
-    chassis.cancelMotion();
-    // Turn to face the point x:45, y:-45. Timeout set to 1000
-    // dont turn faster than 60 (out of a maximum of 127)
-    chassis.turnToPoint(45, -45, 1000, {.maxSpeed = 60});
-    // Turn to face a direction of 90º. Timeout set to 1000
-    // will always be faster than 100 (out of a maximum of 127)
-    // also force it to turn clockwise, the long way around
-    chassis.turnToHeading(90, 1000, {.direction = AngularDirection::CW_CLOCKWISE, .minSpeed = 100});
-    // Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
-    // following the path with the back of the robot (forwards = false)
-    // see line 116 to see how to define a path
-    chassis.follow(example_txt, 15, 4000, false);
-    // wait until the chassis has traveled 10 inches. Otherwise the code directly after
-    // the movement will run immediately
-    // Unless its another movement, in which case it will wait
-    chassis.waitUntil(10);
-    pros::lcd::print(4, "Traveled 10 inches during pure pursuit!");
-    // wait until the movement is done
-    chassis.waitUntilDone();
-    pros::lcd::print(4, "pure pursuit finished!");
+
+
+    // x, y, timeout
+    int autonumber = currAuto;
+    switch (autonumber) {
+        case 1:
+chassis.moveToPoint(0, 10, 2000);
+    break;
+
+    case 2: 
+    forwards(-12000, -12000);
+    pros::delay(200);
+    forwards(0,0);
+    break;
+    }
+
+
 }
 
 /**
@@ -217,11 +221,15 @@ void opcontrol() {
     // loop to continuously update motors
     while (true) {
         // get joystick positions
-        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+        int leftY = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
         // move the chassis with curvature drive
         chassis.arcade(leftY, rightX);
         // delay to save resources
+
+    if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) { // change auto nolan is so awesome and smart and awesome and smart
+      nextState(); 
+	}
 
     if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2)) { // turn lock on and off
       locktoggle = !locktoggle; 
