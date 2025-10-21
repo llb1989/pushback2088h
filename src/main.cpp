352687b4d01d348652622ffc0f3d2491eeacc37a
Nolan Li <nolan.lipishan@gmail.com>
2025-10-20 21:06:21 -0600

    nolan awesome sauce

--- src/main.cpp
+++ src/main.cpp
@@ -129,13 +129,13 @@ lemlib::OdomSensors sensors(
 // input curve for throttle input during driver control
 lemlib::ExpoDriveCurve throttleCurve(3, // joystick deadband out of 127
                                      10, // minimum output where drivetrain will move out of 127
-                                     1.019 // expo curve gain
+                                     1 // expo curve gain
 );
 
 // input curve for steer input during driver control
 lemlib::ExpoDriveCurve steerCurve(3, // joystick deadband out of 127
                                   10, // minimum output where drivetrain will move out of 127
-                                  1.019 // expo curve gain
+                                  1 // expo curve gain
 );
 
 // create the chassis
