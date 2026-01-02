76e028d31475d6faa07f06e659da3b98a5ba3cb7
llb1989 <lyla@westernmech.ca>
2026-01-01 21:00:03 -0700

    leftnside

--- src/auton_paths.cpp
+++ src/auton_paths.cpp
@@ -2,20 +2,57 @@
 #include "auton_paths.hpp"
 //left
 void left_auto(){
-         chassis.setPose(0,0,0);
-         chassis.moveToPoint(0, 36, 2000, {.maxSpeed = 60});
-         pros::delay(10);
-         chassis.turnToHeading(-90, 1000);
-         littlewill.toggle();
-         intakeone(12000);
-         chassis.moveToPoint(-17, 36,1000, {.forwards = true, .maxSpeed = 100, .minSpeed = 50});
-         chassis.moveToPoint(-19, 36,1000, {.forwards = true});
-         pros::delay(700);
-         intakeall(0);
-         pros::delay(100);
-         chassis.moveToPoint(24, 36.5, 1000, {.forwards = false, .maxSpeed = 60});
-         pros::delay(900);
-         intakeall(12000);
+
+    chassis.setPose(0, 0, 0);
+    intakeone(12000);
+     chassis.moveToPoint(0, 22, 500, {.maxSpeed = 90});
+     chassis.turnToHeading(-15, 200);
+     pros::delay(500);
+
+    chassis.moveToPoint(-5.273, 38.067, 1000, {.maxSpeed = 50});
+    pros::delay(500);
+    littlewill.toggle();
+    chassis.turnToPoint(-38, 18,600, {.maxSpeed = 50});
+    chassis.moveToPoint(-38, 18, 1000);
+
+    chassis.turnToHeading(180, 1050);
+    pros::delay(100); 
+    chassis.moveToPoint(-37, -4, 700, {.maxSpeed = 90});
+    chassis.turnToHeading(180, 500);
+    pros::delay(400);
+    chassis.moveToPoint(-37, -5, 500, {.maxSpeed = 90});
+    pros::delay(300);
+    chassis.moveToPoint(-37, 35, 1000, {.forwards = false, .maxSpeed = 70});
+    chassis.moveToPoint(-37, 37, 800, {.forwards = false, .maxSpeed = 70});
+    pros::delay(1800);
+    intakeall(12000);
+    pros::delay(1000); // commit
+    intakeall(-12000);
+    pros::delay(300);
+    intakeall(12000);
+    pros::delay(2400);
+    intakeall(0);
+    chassis.moveToPoint(-58, 18.128, 1000);// og 20.5
+    chassis.turnToHeading(180, 500);
+    chassis.moveToPoint(-53, 50, 2000, {.forwards = false, .maxSpeed = 50});
+    pros::delay(10); //
+
+
+
+        //  chassis.setPose(0,0,0);
+        //  chassis.moveToPoint(0, 36, 2000, {.maxSpeed = 60});
+        //  pros::delay(10);
+        //  chassis.turnToHeading(-90, 1000);
+        //  littlewill.toggle();
+        //  intakeone(12000);
+        //  chassis.moveToPoint(-17, 36,1000, {.forwards = true, .maxSpeed = 100, .minSpeed = 50});
+        //  chassis.moveToPoint(-19, 36,1000, {.forwards = true});
+        //  pros::delay(700);
+        //  intakeall(0);
+        //  pros::delay(100);
+        //  chassis.moveToPoint(24, 36.5, 1000, {.forwards = false, .maxSpeed = 60});
+        //  pros::delay(900);
+        //  intakeall(12000);
 }
 
 //right
@@ -52,7 +89,7 @@ void right_auto() {
     chassis.moveToPoint(22, 18.128, 1000);// og 20.5
     chassis.turnToHeading(180, 500);
     chassis.moveToPoint(26, 47, 2000, {.forwards = false, .maxSpeed = 50});
-    pros::delay(10); 
+    pros::delay(10); //
 }
 
 //sawp
--- src/main.cpp
+++ src/main.cpp
@@ -179,7 +179,7 @@ ASSET(example_txt); // '.' replaced with "_" to make c++ happy
 void autonomous() {
  
     int autonumber = currAuto;
-    switch (3) {
+    switch (2) {
 
         case 1: // forwards
         forwards(8000, 8000);
