b806dba9d9f12a9c2df92cb530c97e275e3f72ec
Nolan Li <nolan.lipishan@gmail.com>
2026-02-10 15:05:41 -0700

    j

--- src/main.cpp
+++ src/main.cpp
@@ -131,7 +131,7 @@ void autonomous() {
     master.print(1, 1, "Y: %f", chassis.getPose().y);
 
     int autonumber = currAuto;
-    switch (9){
+    switch (1){
 
         case 1: // forwards
 test();
